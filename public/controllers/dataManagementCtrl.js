66bf713bf9c1fb7f0e161b64264ad5d14ac17031
florencemayo <florencemayo11@gmail.com>
2016-09-27 19:10:16 +0300

    merging patient feature updated

--- public/controllers/dataManagementCtrl.js
+++ public/controllers/dataManagementCtrl.js
@@ -13,51 +13,27 @@ var dataManagementCtrl = angular.module("dataManagementCtrl",[])
         getAllPatients();
         $scope.searchedValue = " ";
 
-        var capitalizeFirstLetter = function(value){
-            if(angular.isNumber(value) == false){
-                value =value.toLowerCase();
-                value = value.substring(0,1).toUpperCase()+value.substring(1);
-            }
-            return value;
-        }
-
         $scope.existIdentityCode = function(identificationCode){
             $scope.patients = [];
             console.log("identificationCode "+identificationCode);
-            if(angular.isNumber(identificationCode){
-	            console.log("identificationCode "+identificationCode);
-	            for (var i=0; i<$scope.allPatients.length; i++){
-	                    if ($scope.allPatients[i].identification_code == identificationCode){
-	                        $scope.patients.push($scope.allPatients[i]);
-	                     }
-	             }
-	        }else{
-	            $scope.patients=[]; console.log("value isUndefined");
-	        }
-            $scope.showAlert = true;
+            for (var i=0; i < $allPatients.length; i++){
+                if ($allPatients[i].identification_code == identificationCode)
+                    $scope.patients.push($allPatients[i]);
+            }
+	        $scope.showAlert = true; 
         }
 
          $scope.existMobileNumber = function(mobileNumber){
             $scope.patients = [];
             console.log("Number "+mobileNumber);
-            if (checkValue(mobileNumber) == false) {
-            mobileNumber = capitalizeFirstLetter(mobileNumber);
-            for (var i=0; i<$scope.allPatients.length; i++){
-                    if ($scope.allPatients[i].mobile_number == mobileNumber){
-                        $scope.patients.push($scope.allPatients[i]);
-                     }
-                }
-            }else{
-                $scope.patients=[];
+            for (var i=0; i<$allPatients.length; i++){
+                if ($allPatients[i].mobile_number == mobileNumber)
+                    $scope.patients.push($allPatients[i]);
             }
+            $scope.showAlert = true; 
         }
         
-        var checkValue = function(val){
-            return angular.isUndefined(val) || val===null;
-        };
-       
-
-       $scope.enableFields = function(patientAttributes){
+        $scope.enableFields = function(patientAttributes){
             if (patientAttributes == 'identificationCode') {
                 $scope.showIdentificationField = true;
                 $scope.showMobileNumberField = false; 
@@ -68,5 +44,90 @@ var dataManagementCtrl = angular.module("dataManagementCtrl",[])
             $scope.showIdentificationField = false;
             $scope.showMobileNumberField = false;
            }
-        }        
+        }
+
+        $scope.mergePatients =  function(){
+
+        };
+
+        /*var getPatientWithID = function(id){
+            $http.get(API_URL+ "patients/"+id).success(function (data){
+                   patient = data;
+                   console.log(data); 
+                   console.log("Name "+data.first_name);
+            });
+            return patient;
+        }*/
+
+        $scope.checkSimilarAttributes = function(firstID, secondID){
+           $http.get(API_URL+ "patients/"+firstID).success(function (firstData){
+                firstPatient = firstData;
+                $http.get(API_URL+ "patients/"+secondID).success(function (secondData){
+                    secondPatient = secondData;
+                    if (firstPatient.identification_code == secondPatient.identification_code &&
+                        firstPatient.first_name == secondPatient.first_name &&
+                        firstPatient.last_name == secondPatient.last_name &&
+                        firstPatient.mobile_number == secondPatient.mobile_number &&
+                        firstPatient.birthdate == secondPatient.birthdate){
+                        console.log("MERGE PATIENTS");
+                        $scope.textMerge = "MERGE PATIENTS";
+                        //save new patient
+                        $http.post(API_URL+ "patients", {
+                            first_name : firstPatient.first_name,
+                            last_name : firstPatient.last_name,
+                            gender : firstPatient.gender,
+                            birthdate : firstPatient.birthdate,
+                            mobile_number : firstPatient.mobile_number,
+                            nextOfKin : firstPatient.nextOfKin,
+                            street_name : firstPatient.street_name,
+                            district_name : firstPatient.district_name,
+                            region_name : firstPatient.region_name,
+                            country_name : firstPatient.country_name
+                        })
+                        .success(function(data){
+                            console.log("Patient added successful");
+                        });
+                        
+                        //delete first patient
+                        $http.post(API_URL+"patients/delete/"+firstID)
+                            .success(function (data){
+                                console.log(data);
+                                //location.reload();
+                            })
+                            .error(function (data){
+                                console.log(data);
+                                alert('Unable to delete first patient');
+                        });
+
+                        //delete second patient
+                        $http.post(API_URL+"patients/delete/"+secondID)
+                            .success(function (data){
+                                console.log(data);
+                                //location.reload();
+                            })
+                            .error(function (data){
+                                console.log(data);
+                                alert('Unable to delete second patient');
+                        });
+                    }else{
+                        console.log("CONSIDER CHANGING THE IDENTIICATION CODE");
+                        $scope.textMerge = "";
+                    }
+                });
+            });
+        }
+
+        $scope.delete = function(firstID){
+            console.log(firstID);
+            $http.post(API_URL+"patients/delete/" + firstID)
+                .success(function (data){
+                    console.log("Patient Deleted");
+                    location.reload();
+                    console.log("Page refreshed");
+                })
+                .error(function (data){
+                    console.log(data);
+                    alert('Unable to delete second patient');
+            });
+        }
 }]);
